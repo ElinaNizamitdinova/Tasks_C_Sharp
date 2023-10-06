2ac6111ed8be900610af5631237b5d1c4084fa9c
Elina <elina.eldarovna.spb@gmail.com>
2023-10-06 23:09:56 +0300

    Task00

---
+++ Task_00/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A");
+int NumderA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B, предпологаемый квадрат числа A");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+if(NumderA*NumderA==NumberB)
+{
+    Console.Write("Верно, число В является квадратом числа А");
+}
+else
+{
+    Console.Write("Нет, число В не является квадратом числа А");
+}
