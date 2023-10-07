7830549a36e49dbe4777736999fd23fd5bf4023b
Elina <elina.eldarovna.spb@gmail.com>
2023-10-07 14:03:54 +0300

    Task 005

---
+++ Task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number:");
+
+int numder = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Numders:");
+
+for (int i = -numder; i <= numder; i++)
+{
+    Console.WriteLine(i);
+}
