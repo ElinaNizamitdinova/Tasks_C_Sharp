ede8d9f0bea9238d67e87641d27fd4d720d39a0b
Elina <elina.eldarovna.spb@gmail.com>
2023-10-07 14:18:41 +0300

    Task 03

---
+++ Task_03/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Enter number:");
+
+int numder = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Day:");
+
+if (numder==1)
+{
+ Console.WriteLine("Monday");
+}
+
+if (numder==2)
+{
+ Console.WriteLine("Thueday");
+}
+
+
+if (numder==3)
+{
+ Console.WriteLine("Wednesday");
+}
+
+if (numder==4)
+{
+ Console.WriteLine("Thursday");
+}
+
+if (numder==5)
+{
+ Console.WriteLine("Fraiday");
+}
+if (numder==6)
+{
+ Console.WriteLine("Sartuday");
+}
+
+if (numder==7)
+{
+ Console.WriteLine("Sunday");
+}
+if (numder > 7)
+{
+    Console.WriteLine("Error");
+}
+
