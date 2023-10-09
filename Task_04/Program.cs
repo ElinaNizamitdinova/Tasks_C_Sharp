bc52b3951b724fe1046757b96b0ec8b995322223
Elina <elina.eldarovna.spb@gmail.com>
2023-10-09 23:52:06 +0300

    Task04

---
+++ Task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int maximal = a;
+if (maximal < b)
+{
+    maximal = b;
+}
+if (maximal < c)
+{
+    maximal = c;
+}
+System.Console.Write("Максимальное число из трех  ");
+System.Console.Write(maximal);
+
+
