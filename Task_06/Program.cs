7677a63362296b66af94786ad26e97d61584f36b
Elina <elina.eldarovna.spb@gmail.com>
2023-10-10 00:05:32 +0300

    Task06

---
+++ Task_06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int numder = Convert.ToInt32(Console.ReadLine());
+if (numder % 2 == 0)
+{
+    Console.Write($"Число {numder} четное");
+}
+if (numder % 2 > 0);
+{
+    Console.Write($"Число {numder} нечетное");
+}
+
+
