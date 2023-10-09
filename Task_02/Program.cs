43b9fb91be408aa0f974410a11566f8a8db479d4
Elina <elina.eldarovna.spb@gmail.com>
2023-10-09 23:45:15 +0300

    Task02

---
+++ Task_02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");
+}
+else if (firstNumber > secondNumber)
+{
+Console.WriteLine($"Первое число {firstNumber} меньше чем второе число {secondNumber}");
+}
+else if(firstNumber==secondNumber)
+{
+Console.WriteLine($"Введенные числа равны {firstNumber}");
+}
