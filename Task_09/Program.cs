228ca67bb469ecfbd56252e96d9c8dd7eb489b92
Elina <elina.eldarovna.spb@gmail.com>
2023-10-10 00:21:05 +0300

    Task09

---
+++ Task_09/Program.cs
@@ -0,0 +1,12 @@
+﻿int numder = new Random().Next(10, 100);
+Console.WriteLine(numder);
+int first = numder / 10;
+int last = numder % 10;
+if (first > last)
+{
+    Console.Write(first);
+}
+else
+{
+    Console.Write(last);
+}
