da760948244b45841e80fdda36da1dc4d04c566d
Elina <elina.eldarovna.spb@gmail.com>
2023-10-10 11:51:25 +0300

    Task16

--- Task_16/Program.cs
+++ Task_16/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите число и предпологаемый квадрат числа, порядок не важен");
-int numder1 = Convert.ToInt32(Console.ReadLine());
-int numder2 = Convert.ToInt32(Console.ReadLine());
-if ((numder2 * numder2 == numder1) || (numder2 == numder1 * numder1))
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if ((number2 * number2 == number1) || (number2 == number1 * number1))
 {
     Console.Write("Yes");
 }
