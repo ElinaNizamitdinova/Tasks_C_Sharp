fdeb3882ac1692c35c402a303e35694abaa64f11
Elina <elina.eldarovna.spb@gmail.com>
2023-10-10 12:47:28 +0300

    Task13

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+if (value < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int valueSecond = (value / 100) % 10;
+    Console.WriteLine(valueSecond);
+}
