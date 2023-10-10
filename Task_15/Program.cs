532ed057bc8e354745953da48e2b3cf65188f18a
Elina <elina.eldarovna.spb@gmail.com>
2023-10-10 12:55:46 +0300

    Task15

---
+++ Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    Console.Write("Yes");
+}
+else if (day > 7)
+{
+    Console.Write("Такого дня недели нет");
+}
+else
+{
+    Console.Write("No");
+}
