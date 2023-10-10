526b298ea83375489abfa6c48a18c9a1e055daa9
Elina <elina.eldarovna.spb@gmail.com>
2023-10-10 12:26:43 +0300

    Task12

---
+++ Task_12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int Anumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чиисло, которому предположительно кратно первое число");
+int Bnumber = Convert.ToInt32(Console.ReadLine());
+if (Anumber%Bnumber==0){
+    Console.WriteLine("Кратно");
+}
+else{
+    int ost=Anumber%Bnumber;
+    Console.WriteLine($"Некратно, статок {ost}");
+}
