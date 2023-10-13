16184243a882444e5389d614b698f41d8bfdbd30
Elina <elina.eldarovna.spb@gmail.com>
2023-10-13 12:46:10 +0300

    Task00

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,12 +1,5 @@
 ﻿Console.WriteLine("Введите число A");
 int NumderA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B, предпологаемый квадрат числа A");
-int NumberB = Convert.ToInt32(Console.ReadLine());
-if(NumderA*NumderA==NumberB)
-{
-    Console.Write("Верно, число В является квадратом числа А");
-}
-else
-{
-    Console.Write("Нет, число В не является квадратом числа А");
-}
+Console.Write("Квадрат числа А равен ");
+Console.WriteLine(NumderA * NumderA);
+ 
