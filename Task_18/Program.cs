c03160fca88baf7391cb2dce7a4ecb9d74f08c77
Elina <elina.eldarovna.spb@gmail.com>
2023-10-13 12:15:00 +0300

    Task18

---
+++ Task_18/Program.cs
@@ -0,0 +1,22 @@
+﻿string f(int x)
+{
+    if (x == 1)
+    {
+        return "x > 0 && y > 0";
+    }
+    else if (x == 3)
+    {
+        return "x < 0 && y < 0";
+    }
+    else if (x == 4)
+    {
+        return "x > 0 && y < 0";
+    }
+    return "x < 0 && y > 0";
+}
+
+
+
+Console.WriteLine("Введите номер четверти");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(x));
