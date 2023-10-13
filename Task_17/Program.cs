1ab6cd6c03924ff0669bb381a810255a0af89ded
Elina <elina.eldarovna.spb@gmail.com>
2023-10-13 12:03:27 +0300

    Task17

---
+++ Task_17/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+string f(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return "I";
+    }
+    else if (x < 0 && y < 0)
+    {
+        return "III";
+    }
+    else if (x > 0 && y < 0)
+    {
+        return "IV";
+    }
+    return "II";
+}
+
+
+
+Console.WriteLine("Введите значение x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение y");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(x, y));
