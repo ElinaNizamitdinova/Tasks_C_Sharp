cfd9f2e1c3f036021fa40f14c5a0c7ecc955ff7d
Elina <elina.eldarovna.spb@gmail.com>
2023-10-13 15:12:31 +0300

    Task19

---
+++ Task_19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int Numder5 = Convert.ToInt32(Console.ReadLine());
+if(Numder5%10==Numder5/10000){
+    if ((Numder5/1000)%10==(Numder5/10)%10){
+        Console.WriteLine("Число является полиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Число не является полиндромом");
+}
