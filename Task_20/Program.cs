ba427f71417b74cde72ff941f05f75bc3d733852
Elina <elina.eldarovna.spb@gmail.com>
2023-10-14 22:56:01 +0300

    Task20

---
+++ Task_20/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите координаты первой точки, сначало x, затем y");
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки, сначало x, затем y");
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+double SegmentLenght2 =Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2);
+double SegmentLenght = Math.Sqrt(SegmentLenght2);
+Console.WriteLine(SegmentLenght);
