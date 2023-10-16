1bcf71737d3eeebe5e601cc21633e3e83d75e3df
Elina <elina.eldarovna.spb@gmail.com>
2023-10-16 11:41:41 +0300

    Task24

---
+++ Task_24/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите число");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Summ(int a)
+{
+    int summ = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        summ = summ + i;
+    }
+    return summ;
+}
+int userResult = Summ(userA);
+System.Console.WriteLine(userResult);
