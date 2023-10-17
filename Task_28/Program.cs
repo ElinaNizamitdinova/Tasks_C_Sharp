81fd771da23d2e767adfb460537c63f4e633c9d5
Elina <elina.eldarovna.spb@gmail.com>
2023-10-17 11:14:12 +0300

    Task28

---
+++ Task_28/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите число");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Mult(int a)
+{
+    int mult = 1;
+
+    for (int i = 1; i <= a; i++)
+    {
+        mult= mult*i;
+    }
+    return mult;
+}
+int userResult = Mult(userA);
+System.Console.WriteLine(userResult);
