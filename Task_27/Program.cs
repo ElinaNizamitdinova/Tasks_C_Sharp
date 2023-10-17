3f332bd060e683d79792b1624aa577e93e4da2e4
Elina <elina.eldarovna.spb@gmail.com>
2023-10-17 12:05:22 +0300

    Task27

---
+++ Task_27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberSumm= 0;
+while (number > 0)
+{
+    int numberA = number%10;
+    numberSumm= numberSumm + numberA;
+    number = number/10;
+}
+Console.WriteLine(numberSumm);
