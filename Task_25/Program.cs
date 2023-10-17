26e0a9af5ab267577c8b1854d0e925573fbd5212
Elina <elina.eldarovna.spb@gmail.com>
2023-10-17 11:53:52 +0300

    Task25

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int AtoTheBPower= 1;
+while (numberB > 0)
+{
+    AtoTheBPower= AtoTheBPower * numberA;
+    numberB = numberB - 1;
+}
+Console.WriteLine(AtoTheBPower);
