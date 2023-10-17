05d119a71cc3c5cc87d5e728cba88809a396e81b
Elina <elina.eldarovna.spb@gmail.com>
2023-10-17 11:13:44 +0300

    Task26

---
+++ Task_26/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+string userNumb = Console.ReadLine();
+int LenghtNum(string Num)
+{
+
+    int length = userNumb.Length;
+    return length;
+}
+int LenghtNumb = LenghtNum(userNumb);
+Console.WriteLine(LenghtNumb);
+ 
+
