6d5a59deebbf8165d49ac39ebe38b06dceb2e139
Elina <elina.eldarovna.spb@gmail.com>
2023-10-23 00:38:53 +0300

    Task35

---
+++ Task_35/Program.cs
@@ -0,0 +1,51 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своем решении 
+// сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+int[] RandomArray(int size, int maxValue, int minValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}\t");
+    }
+}
+
+int Range(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>10 & array[i]<100)
+        {
+            count++;
+        };
+    }
+    return count;
+}
+Console.WriteLine("Введите количество элементов в массиве");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное значение в массиве");
+int maxi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Минимальное значение в массиве");
+int mini = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-------");
+int[] Mas = RandomArray(size,maxi,mini);
+PrintArray(Mas);
+int count = Range(Mas);
+Console.WriteLine("");
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99] равно {count} ");
