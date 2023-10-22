a8cac13aa9767fff73a8b64db3679cda78cec46e
Elina <elina.eldarovna.spb@gmail.com>
2023-10-22 16:09:39 +0300

    Task29

---
+++ Task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int[] GetNewArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i<size;i++ )
+    {
+        array[i]=new Random().Next(0,100);
+
+    }
+
+    return array;
+}
+
+void WriteArray(int size,int[]array){
+    for(int i = 0; i<size;i++){
+        Console.Write($"{array[i]},");
+    }
+
+}
+Console.WriteLine("Введите желаймую длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] NewArray= GetNewArray(size);
+WriteArray(size,NewArray);
