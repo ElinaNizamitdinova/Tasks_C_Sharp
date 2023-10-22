388af2e3302b8f60cc23144bc26ad18a6bb58a35
Elina <elina.eldarovna.spb@gmail.com>
2023-10-23 00:17:43 +0300

    Task32

---
+++ Task_32/Program.cs
@@ -0,0 +1,42 @@
+﻿//  Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] RandomArray(int size, int maxValue, int minValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"{arr[i]}");
+    }
+}
+
+void ReversArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+
+    PrintArray(array);
+}
+Console.WriteLine("Введите количество элементов в массиве");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное значение в массиве");
+int maxi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Минимальное значение в массиве");
+int mini = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-------");
+int[] Mas = RandomArray(size,maxi,mini);
+PrintArray(Mas);
+Console.WriteLine("-------");
+ReversArray(Mas);
