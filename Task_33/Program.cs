ff5c911d0bdb19fa2dccc78fc2c8b2164b48f908
Elina <elina.eldarovna.spb@gmail.com>
2023-10-22 18:59:24 +0300

    Task31

---
+++ Task_33/Program.cs
@@ -0,0 +1,42 @@
+﻿//  Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] GetNewArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 3);
+
+    }
+
+    return array;
+}
+
+
+void Presence(int size, int[] array, int number)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == number)
+        {
+            Console.WriteLine("Yes");
+            return;
+        }
+        
+    }
+    Console.WriteLine("No");
+}
+
+Console.WriteLine("Введите длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int[] Array = GetNewArray(size);
+
+Presence(size, Array, Number);
+
