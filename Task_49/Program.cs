889b609bf467c63815734d922f6cd3b9ade0c70f
Elina <elina.eldarovna.spb@gmail.com>
2023-10-26 15:32:50 +0300

    Task49

---
+++ Task_49/Program.cs
@@ -0,0 +1,52 @@
+﻿int[,] RandomArray(int sizeline, int sizecolumns, int maxValue, int minValue)
+{
+    int[,] array = new int[sizeline, sizecolumns];
+    for (int i = 0; i < sizeline; i++)
+    {
+        for (int j = 0; j < sizecolumns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] PositiveIndex(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < arr.GetLength(1); j += 2)
+        {
+            arr[i, j] = arr[i, j] * arr[i, j];
+        }
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите количество элементов в массиве");
+int sizeline = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество элементов в массиве");
+int sizecolumns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное значение в массиве");
+int maxi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Минимальное значение в массиве");
+int mini = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-------");
+int[,] Userarray = RandomArray(sizeline, sizecolumns, maxi, mini);
+PrintArray(Userarray);
+Console.WriteLine("-------");
+Userarray = PositiveIndex(Userarray);
+PrintArray(Userarray);
+Console.ReadKey();
