201b74ff45b3b3540e2200bc32a4cf9013db490b
Elina <elina.eldarovna.spb@gmail.com>
2023-10-26 16:49:00 +0300

    Task44

---
+++ Task_44/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int first = 0;
+int second = 1;
+if (N>=1){
+    Console.Write(first);
+}
+if (N>=2){
+    Console.Write(","+second);
+}
+for (int i = 3; i <= N; i++)
+{
+    int next = first+second;
+    Console.Write(","+next);
+    first=second;
+    second=next;
+}
+Console.WriteLine();
