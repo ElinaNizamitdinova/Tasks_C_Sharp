2babe0553142ca41345b4a6a0a14cc6682bab082
Elina <elina.eldarovna.spb@gmail.com>
2023-11-14 14:06:17 +0300

    Task55

---
+++ Task_55/Program.cs
@@ -0,0 +1,63 @@
+﻿// 55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// В итоге получается вот такой массив:
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+
+int[,] RandomArray(int sizeline, int sizecolumns, int maxValue, int minValue)
+{
+    int[,] array = new int[sizeline, sizecolumns];
+    for (int i = 0; i < sizeline; i++)
+    {
+        for (int j = 0; j < sizecolumns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество колон и строк");
+int countcolumn = Convert.ToInt32(Console.ReadLine());
+int countline = Convert.ToInt32(Console.ReadLine());
+if (countcolumn != countline)
+{
+    Console.WriteLine("Ошибка");
+}
+int[,] ArrayUser = RandomArray(countline, countcolumn, 10, 0);
+PrintArray(ArrayUser);
+Console.WriteLine("------------------");
+int[,] ChangeLineColumn(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j <i ; j++)
+        {
+            int time = Array[i, j];
+            Array[i, j] = Array[j, i];
+            Array[j, i] = time;
+        }
+    }
+    PrintArray(Array);
+    return Array;
+}
+int[,] newArray = ChangeLineColumn(ArrayUser);
+Console.ReadKey();
