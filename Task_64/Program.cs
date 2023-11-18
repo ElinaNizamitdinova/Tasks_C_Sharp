f1570b36189964d96e8a3ff201033e82a45d0ee3
Elina <elina.eldarovna.spb@gmail.com>
2023-11-18 20:25:56 +0300

    Task64

---
+++ Task_64/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+
+void IntN(int num)
+{
+    if (num > 0)
+    {
+        Console.Write($" {num}");
+        if (num == 1)
+        {
+            Console.Write(" ");
+        }
+
+        else Console.Write(",");
+        IntN(num - 1);
+    }
+}
+Console.Write($"N={numberN} =>");
+IntN(numberN);
+
+Console.ReadKey();
