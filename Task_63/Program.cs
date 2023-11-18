55751e20c1eb42408c9b445a931b3ba8f20c9a10
Elina <elina.eldarovna.spb@gmail.com>
2023-11-18 15:57:09 +0300

    Task63

---
+++ Task_63/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число N");
+int numderN = Convert.ToInt32(Console.ReadLine());
+
+
+
+void IntN(int num, int current = 1)
+{
+    if (num < current) return;
+    else
+    {
+        Console.Write(current + " ");
+        IntN(num, current + 1);
+    }
+}
+
+IntN(numderN);
+
+Console.ReadKey();
