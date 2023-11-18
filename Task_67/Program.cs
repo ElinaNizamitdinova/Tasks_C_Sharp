d48b2ce743e2904f05e4e128f7a109729855cfbe
Elina <elina.eldarovna.spb@gmail.com>
2023-11-18 21:01:02 +0300

    Task67

---
+++ Task_67/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SummOfNumbers(int num,int summ=0){
+    if (num>0){
+    summ= summ+(num%10);
+    SummOfNumbers(num/10,summ);
+    }
+    else Console.WriteLine($"Сумма цифр в числе = {summ}");
+    
+}
+SummOfNumbers(number);
+Console.ReadKey();
