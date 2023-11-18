9a3c1c990c64ebb225bcd7083a92d3b5dceede02
Elina <elina.eldarovna.spb@gmail.com>
2023-11-18 21:54:17 +0300

    Task68

---
+++ Task_68/Program.cs
@@ -0,0 +1,16 @@
+﻿int Ack(int n, int m)
+{
+
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Ack(n - 1, 1);
+    if (n > 0 && m > 0) return Ack(n - 1, Ack(n, m - 1));
+    return Ack(n, m);
+}
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int M = Convert.ToInt32(Console.ReadLine());
+int ackNM = Ack(N, M);
+Console.WriteLine(ackNM);
+Console.ReadKey();
