e5d52714185c7024de96e7a59161f2d4d6f5e6d9
Elina <elina.eldarovna.spb@gmail.com>
2023-11-18 22:08:59 +0300

    Task69

---
+++ Task_69/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+void  AToTheBPower(int a, int b,int value)
+{
+    if (b > 1){
+        value *= a;
+        AToTheBPower(a, b - 1,value);
+    }
+    else{
+        Console.WriteLine(value);
+    }
+
+}
+AToTheBPower(numberA, numberB,numberA);
+Console.ReadKey();
